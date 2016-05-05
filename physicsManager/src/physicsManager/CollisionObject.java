f73fc34d00e65e9440ed2daaff665db0462b314a
Pressian Abarov <pressian.abarov@gmail.com>
2016-05-05 16:52:42 +0200

    intersects and boundingboxes

--- physicsManager/src/physicsManager/CollisionObject.java
+++ physicsManager/src/physicsManager/CollisionObject.java
@@ -2,16 +2,18 @@ package physicsManager;
 
 import java.util.ArrayList;
 
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.math.collision.BoundingBox;
 
 public class CollisionObject {
 	
-	public ArrayList<Object> triangles;
+	public ArrayList<Vector3[]> triangles;
+	public BoundingBox boundingBox;
 	
-	
-	// change triangles type?
-	public CollisionObject(ArrayList<Object> triangles) {
-		
+	public CollisionObject(ArrayList<Vector3[]> triangles) {
+		this.triangles = triangles;
 	}
+	
+	
 	
 }
